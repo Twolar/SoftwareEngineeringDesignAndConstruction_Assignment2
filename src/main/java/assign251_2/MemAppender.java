ebc98cc534c16b96a3528144f60942ed164719ca
Taylor Bennett <t4ybennett@gmail.com>
2019-10-03 12:25:30 +1300

    Created MemAppender Singleton Test & Functionality

--- src/main/java/assign251_2/MemAppender.java
+++ src/main/java/assign251_2/MemAppender.java
@@ -3,5 +3,19 @@ package assign251_2;
 
 public class MemAppender 
 {
- 
+    private static MemAppender memAppender = null;
+    public String test;
+
+    private MemAppender(){
+        test = "test singleton";
+    }
+    
+    public static MemAppender getMemAppenderInstance(){
+        if (memAppender == null){
+            memAppender = new MemAppender();
+        }
+        return memAppender;
+    }
+
+    
 }
--- src/test/java/assign251_2/AppTest.java
+++ src/test/java/assign251_2/AppTest.java
@@ -1,9 +1,43 @@
 package assign251_2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * Unit test for simple MemAppender & VelocityLayout
  */
 public class AppTest 
 {
+    private String[] loggingEventsArray;
+    @BeforeEach
+    void initialiaseLoggingEvents(){
+        loggingEventsArray = new String[3];
+        loggingEventsArray[0] = "TEST ONE";
+        loggingEventsArray[1] = "TEST TWO";
+        loggingEventsArray[2] = "TEST THREE";  
+    }
+
+    @Test
+    void testSingletonMemAppenderFunctionality(){
+        MemAppender testInstance1 = MemAppender.getMemAppenderInstance();
+        MemAppender testInstance2 = MemAppender.getMemAppenderInstance();
+
+        assertEquals(testInstance1, testInstance2, "two instances are different, singleton not working");
+    }
+
+    @Test
+    void testMemAppenderEventListCreation(){
+        assert(true);
+    }
+
+    @AfterEach
+    void nullifyLoggingEvents(){
+        loggingEventsArray = null;
+    }
 }
